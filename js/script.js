c9a2b67e2cc077169c10b74a4c84baa90115b9ad
IamCoy <jacoyiaj@yahoo.com>
2021-09-03 11:08:16 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let petname = prompt("Name your pet.");
+
+let happiness = 0;
+let energy= 0;
+
+let action = prompt("feed, pet or walk?")
+  if (action == "feed"); {
+    energy += 2;}
+  if (action == "pet"); {
+    happiness += 1;}
+  if (action == "walk"); {
+    happiness += 2;
+    energy +- 1;}
+while (action == "walk" && energy === 0); {
+  happiness += 0;
+  energy += 0;}
+    
+
+  
+
