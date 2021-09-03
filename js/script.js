3fc4320d0af6484ef1fa2d011a0cc0fb20e6b31e
IamCoy <jacoyiaj@yahoo.com>
2021-09-03 11:55:01 -0400

    mybest

--- js/script.js
+++ js/script.js
@@ -4,17 +4,22 @@ let happiness = 0;
 let energy= 0;
 
 let action = prompt("feed, pet or walk?")
+for (let i = 1; i < 6; i++)
   if (action == "feed"); {
     energy += 2;}
   if (action == "pet"); {
     happiness += 1;}
   if (action == "walk"); {
     happiness += 2;
-    energy +- 1;}
-while (action == "walk" && energy === 0); {
+    energy -= 1;}
+
+  while (action == "walk" && energy === 0); {
   happiness += 0;
-  energy += 0;}
-    
+  energy += 0;}  
 
   
+console.log(energy)
+console.log(happiness)
 
+
+console.log(petname + ' ' + "has" + ' ' + happiness + ' ' + "happiness" + ' ' + "and" + ' ' + energy + ' ' + "energy");
