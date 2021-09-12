348a0d9aad532673adcadd87077a3e575a3fc308
IamCoy <jacoyiaj@yahoo.com>
2021-09-12 17:26:51 -0400

    final

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ let energy= 0;
 
 for (let i = 1; i < 7; i++) {
   let action = prompt("feed, pet or walk?");
- if (action === "feed") {
+   if (action === "feed") {
     energy += 2;
   } else if (action === "pet") {
     happiness += 1;
-  } else if (action === "walk") {
-    happiness += 2;
-    energy -= 1;
-  }
+  } else if (action === "walk") { 
+     if (action === "walk" && energy === 0) {
+        alert("not enough energy to enjoy a walk");
+        happiness += 0;
+        energy += 0;} 
+     else {
+        happiness += 2;
+        energy -= 1;}
+    }  
 }
-
-  
 console.log(energy)
 console.log(happiness)
 
-
-console.log(petname + " has " + happiness  + " happiness " + " and " + energy + " energy ")
+console.log(petname + " has " + happiness  + " happiness and " + energy + " energy ")
